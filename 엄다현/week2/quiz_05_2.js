025c715b43a057a7fb9f7626b4da41e9deb67553
dahyun1060 <dahyun1060@naver.com>
2025-09-20 21:40:50 +0900

    Feat: 2주차 실습

---
+++ 엄다현/week2/quiz_05_2.js
@@ -0,0 +1,19 @@
+function printAvgScore(students) {
+  for (let name in students) {
+    let sum = 0;
+    const { scores } = students[name];
+
+    for (let score of scores) {
+      sum += score;
+    }
+
+    const average = sum / scores.length;
+    console.log(`${name}: ${average}`);
+  }
+}
+
+printAvgScore({
+  이정환: { hobby: "테니스", scores: [10, 20, 30, 40, 50] },
+  김효빈: { hobby: "테니스", scores: [90, 80, 30, 70, 50] },
+  홍길동: { hobby: "의적", scores: [100, 100, 20, 20, 50] },
+});
---
+++ 엄다현/week2/quiz_06_2.js
@@ -0,0 +1,34 @@
+/* 1. 주문하는 기능 (수정 X) */
+function receiveOrder() {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      console.log("주문 접수가 완료되었습니다.");
+      resolve("주문번호: 123");
+    }, 2000)
+  );
+}
+
+/* 2. 주문을 처리하는 기능 */
+function processOrder(orderId) {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      console.log(`[${orderId}] 주문이 처리 되었습니다.`);
+      resolve(orderId);
+    }, 2000)
+  );
+}
+
+/* 3. 주문을 취소하는 기능 */
+function cancelOrder(orderId) {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      console.log(`[${orderId}] 주문이 취소되었습니다`);
+      resolve();
+    }, 1500)
+  );
+}
+
+receiveOrder()
+  .then((orderId) => processOrder(orderId))
+  .then((orderId) => cancelOrder(orderId))
+  .catch((err) => console.error(err));
